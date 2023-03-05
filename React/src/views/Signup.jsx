9a9afdc58bb6c37adc0668a9c767da1192dacb2c
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-03-05 04:07:02 +0100

    05.03.2023 at 04:06

--- React/src/views/Signup.jsx
+++ React/src/views/Signup.jsx
@@ -1,10 +1,41 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/contextProvider"
 
-export default function Signup () {
+export default function Signup() {
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
+    const {setUser, setToken} = useStateContext()
 
     const onSubmit = (ev) => {
         ev.preventDefault()
-    } 
+
+        const payload = {
+            name : nameRef.current.value,
+            email : emailRef.current.value,
+            password : passwordRef.current.value, 
+            password_confirmation : passwordConfirmationRef.current.value,
+        }
+
+        axiosClient.post('/signup', payload)
+            .then(({data}) => {
+                setUser(data.user)
+                setToken(data.token)
+            })
+
+            .catch(err => {
+                const response = err.response
+
+                if (response && response.status === 422) {
+                    console.log(response.data.errors )
+                }
+            })
+    }
 
     return (
 
@@ -15,12 +46,11 @@ export default function Signup () {
                     <h1 className="title">
                         Créer un compte
                     </h1>
+                    <input ref={emailRef} type="text" placeholder="Nom complet" />
+                    <input ref={nameRef} type="email" placeholder="Email" />
 
-                    <input type="email" placeholder="Email"/>
-                    <input type="text" placeholder="Nom complet"/>
-
-                    <input type="password" placeholder="Mot de passe"/>
-                    <input type="password" placeholder="Confirmer le mot de passe"/>
+                    <input ref={passwordRef} type="password" placeholder="Mot de passe" />
+                    <input ref={passwordConfirmationRef} type="password" placeholder="Confirmer le mot de passe" />
                     <button className="btn btn-block">Enregister le compte</button>
 
                     <p className="message">
